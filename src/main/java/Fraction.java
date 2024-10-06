c91eaddd268f3786ede399bfd660c0821b2f41aa
Nikita Ryabinskii <ryabinskiyna@gmail.com>
2024-10-06 14:11:30 +0300

    initial commit

---
+++ src/main/java/Fraction.java
@@ -0,0 +1,51 @@
+public class Fraction {
+    private final int numerator;
+    private final int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new IllegalArgumentException("Знаменатель не может быть равен нулю.");
+        }
+        if (denominator < 0) {
+            throw new IllegalArgumentException("Знаменатель не может быть отрицательным.");
+        }
+
+        int gcd = gcd(numerator, denominator);
+        this.numerator = numerator / gcd;
+        this.denominator = denominator / gcd;
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return Math.abs(a);
+        }
+        return gcd(b, a % b);
+    }
+
+    public Fraction sum(Fraction other) {
+        int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction minus(Fraction other) {
+        int newNumerator = this.numerator * other.denominator - other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction sum(int number) {
+        int newNumerator = this.numerator + number * this.denominator;
+        return new Fraction(newNumerator, this.denominator);
+    }
+
+    public Fraction minus(int number) {
+        int newNumerator = this.numerator - number * this.denominator;
+        return new Fraction(newNumerator, this.denominator);
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Fraction f1 = new Fraction(1, 3);
+        Fraction f2 = new Fraction(2, 5);
+        Fraction f3 = new Fraction(7, 8);
+
+        Fraction result = f1.sum(f2).sum(f3).minus(5);
+
+        System.out.println("Результат: " + result);
+    }
+}
